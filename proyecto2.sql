06f84f9dc9f5ed25701372aa20b71427b6689b91
Mónica Salvatierra <sal22249@uvg.edu.gt>
2024-04-09 13:53:05 -0600

    unificar todas las instrucciones en un solo archivo

--- proyecto2.sql
+++ proyecto2.sql
@@ -121,3 +121,28 @@ CREATE TABLE Queja ( -- Permite a los clientes presentar quejas, que se registra
     FOREIGN KEY (id_personal) REFERENCES Personal(id_personal),
     FOREIGN KEY (id_item) REFERENCES Item(id_item)
 );
+
+--Inserción de datos
+--(Faltan agregar más)
+
+--Area--
+INSERT INTO Area (nombre, fumadores, ) VALUES ('Terraza', TRUE);
+INSERT INTO Area (nombre, fumadores, ) VALUES ('Salón principal', FALSE);
+INSERT INTO Area (nombre, fumadores, ) VALUES ('Salón 2', FALSE);
+INSERT INTO Area (nombre, fumadores, ) VALUES ('Salón 3', FALSE);
+INSERT INTO Area (nombre, fumadores, ) VALUES ('Pérgola', FALSE);
+
+--Mesa--
+INSERT INTO Mesa (capacidad, movil, id_area) VALUES (4, TRUE, 1);
+INSERT INTO Mesa (capacidad, movil, id_area) VALUES (2, FALSE, 2);
+INSERT INTO Mesa (capacidad, movil, id_area) VALUES (6, TRUE, 3);
+
+--Cliente--
+INSERT INTO Cliente (nit, nombre, direccion) VALUES ('123456-K', 'Juan Pérez', 'Calle La Laguna 526');
+INSERT INTO Cliente (nit, nombre, direccion) VALUES ('654321-3', 'Ana López', 'Avenida Sampedrana 437');
+
+--Personal--
+INSERT INTO Personal (nombre, rol) VALUES ('Carlos Martínez', 'Gerente');
+INSERT INTO Personal (nombre, rol) VALUES ('Sofía Gómez', 'Chef');
+INSERT INTO Personal (nombre, rol) VALUES ('Daniel Barrios', 'Mesero');
+INSERT INTO Personal (nombre, rol) VALUES ('Ximena Garcia', 'Host');
