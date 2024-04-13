9ef6f0566d9b0f67a51257d7763b8aaf92a1ae22
JSPA <per22318@uvg.edu.gt>
2024-04-13 13:15:29 -0600

    agregar mesero a la tabla de mesero, si es que el rol es un Mesero

--- app.js
+++ app.js
@@ -1,91 +1,103 @@
 //Agregar evento para el DOM
-document.addEventListener("DOMContentLoaded", function (){
-    //Llamado de todas las funciones
-    botonMesa();
-    mostrarAlertaExito();
-    activarBotonEditarPersonal();
-    activarBotonEliminarPersonal();
-})
+document.addEventListener("DOMContentLoaded", function () {
+  //Llamado de todas las funciones
+  botonMesa();
+  mostrarAlertaExito();
+  activarBotonEditarPersonal();
+  activarBotonEliminarPersonal();
+});
 
+function botonMesa() {
+  //Seleccionar botones
+  const botonesRojo = document.querySelectorAll(".boton-rojo");
+  const botonesVerde = document.querySelectorAll(".boton-verde");
+  botonesRojo.forEach((boton) => {
+    boton.addEventListener("click", function () {
+      const seccion = boton.closest(".mesas-seccion");
 
-function botonMesa (){
-    //Seleccionar botones
-    const botonesRojo = document.querySelectorAll(".boton-rojo");
-    const botonesVerde = document.querySelectorAll(".boton-verde");
-    botonesRojo.forEach(boton => {
-        boton.addEventListener("click", function() {
-
-            const seccion = boton.closest(".mesas-seccion");
-
-            //Si no contiene la clase mesas-seccion-rojo agregarla
-            if (!seccion.classList.contains("mesas-seccion-rojo")) {
-                seccion.classList.add("mesas-seccion-rojo");
-            } 
-            
-        });
+      //Si no contiene la clase mesas-seccion-rojo agregarla
+      if (!seccion.classList.contains("mesas-seccion-rojo")) {
+        seccion.classList.add("mesas-seccion-rojo");
+      }
     });
+  });
 
-    botonesVerde.forEach(botonVerde => {
-        //Seleccionar boton verde
-        botonVerde.addEventListener("click", function() {
-            const seccion = botonVerde.closest(".mesas-seccion");
+  botonesVerde.forEach((botonVerde) => {
+    //Seleccionar boton verde
+    botonVerde.addEventListener("click", function () {
+      const seccion = botonVerde.closest(".mesas-seccion");
 
-            //Si tiene la clase mesas-seccion-rojo eliminarla y agregar la clase mesas-seccion
-            if (seccion.classList.contains("mesas-seccion-rojo")) {
-                seccion.classList.remove("mesas-seccion-rojo");
-                seccion.classList.add("mesas-seccion");
-            }
-        });
+      //Si tiene la clase mesas-seccion-rojo eliminarla y agregar la clase mesas-seccion
+      if (seccion.classList.contains("mesas-seccion-rojo")) {
+        seccion.classList.remove("mesas-seccion-rojo");
+        seccion.classList.add("mesas-seccion");
+      }
     });
+  });
 }
 
 function mostrarAlertaExito() {
-    const alertaExito = document.querySelector('.alerta.exito');
-    if (alertaExito) {
-        alertaExito.style.display = 'block'; // Mostrar la alerta
+  const alertaExito = document.querySelector(".alerta.exito");
+  if (alertaExito) {
+    alertaExito.style.display = "block"; // Mostrar la alerta
 
-        // Eliminar la alerta despues de 3 segundos
-        setTimeout(function() {
-            alertaExito.style.display = 'none';
-        }, 3000);
-    }
+    // Eliminar la alerta despues de 3 segundos
+    setTimeout(function () {
+      alertaExito.style.display = "none";
+    }, 3000);
+  }
 }
 
 function activarBotonEditarPersonal() {
-    //Seleccionar botones
-    const botonesEditarPersonal = document.querySelectorAll(".personal-editar");
+  //Seleccionar botones
+  const botonesEditarPersonal = document.querySelectorAll(".personal-editar");
 
-    botonesEditarPersonal.forEach(boton => {
-        boton.addEventListener("click", function() {
-            // Obtener el id del personal desde el botón
-            const idPersonal = boton.dataset.id;
+  botonesEditarPersonal.forEach((boton) => {
+    boton.addEventListener("click", function () {
+      // Obtener el id del personal desde el botón
+      const idPersonal = boton.dataset.id;
 
-            if (idPersonal) {
-                // Redireccionar a editar-personal.php con el id del personal
-                window.location.href = `editar-personal.php?id=${idPersonal}`;
-            } else {
-                console.error("No se encontró el id del personal");
-            }
-        });
+      if (idPersonal) {
+        // Redireccionar a editar-personal.php con el id del personal
+        window.location.href = `editar-personal.php?id=${idPersonal}`;
+      } else {
+        console.error("No se encontró el id del personal");
+      }
     });
+  });
 }
 
 function activarBotonEliminarPersonal() {
-    //Seleccionar botones
-    const botonesEliminarPersonal = document.querySelectorAll(".personal-borrar");
+  //Seleccionar botones
+  const botonesEliminarPersonal = document.querySelectorAll(".personal-borrar");
 
-    botonesEliminarPersonal.forEach(boton => {
-        boton.addEventListener("click", function() {
-            // Obtener el id del personal desde el botón
-            const idPersonal = boton.dataset.id;
+  botonesEliminarPersonal.forEach((boton) => {
+    boton.addEventListener("click", function () {
+      // Obtener el id del personal desde el botón
+      const idPersonal = boton.dataset.id;
 
-            if (idPersonal) {
-                // Redireccionar a editar-personal.php con el id del personal
-                window.location.href = `borrar-personal.php?id=${idPersonal}`;
-            } else {
-                console.error("No se encontró el id del personal");
-            }
-        });
+      if (idPersonal) {
+        // Redireccionar a editar-personal.php con el id del personal
+        window.location.href = `borrar-personal.php?id=${idPersonal}`;
+      } else {
+        console.error("No se encontró el id del personal");
+      }
     });
+  });
 }
 
+function mostrarOcultarCampo() {
+  const rol = document.getElementById("rol");
+
+  rol.addEventListener("change", () => {
+    const valor = rol.value;
+    let campoMesero = document.querySelector(".campoMesero");
+
+    if (valor === "Mesero") {
+      if (campoMesero.classList.contains("campoMesero")) {
+        campoMesero.classList.remove("campoMesero");
+        campoMesero.classList.add("campoMeseroVista");
+      }
+    }
+  });
+}
--- pages/form-personal.php
+++ pages/form-personal.php
@@ -9,29 +9,56 @@ $db = conectarBD();
 $errores = [];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = $_POST['nombre']; 
+    $nombre = $_POST['nombre'];
     $rol = $_POST['rol'];
+    $id_area = intval($_POST['area']);
 
-    if(!$nombre){
+    if (!$nombre) {
         $errores[] = "El nombre es obligatorio";
     }
 
-    if(!$rol){
+    if (!$rol) {
         $errores[] = "El rol es obligatorio";
-
     }
 
-    if(empty($errores)) {
+    if (empty($errores)) {
         $query = "INSERT INTO personal (nombre, rol) VALUES ('$nombre', '$rol')";
         $stmt = $db->prepare($query);
         $stmt->execute();
-        
-        if($stmt ->rowCount() > 0){
-            header('Location: /pages/personal.php?success=1');
+
+        if ($rol == 'Mesero') {
+
+            if ($stmt->rowCount() > 0) {
+
+                $id_personal = $db->lastInsertId();
+
+
+                $meseroQuery = "INSERT INTO mesero (id_personal, nombre, id_area_asignada) VALUES ('$id_personal', '$nombre', '$id_area')";
+                $stmtMesero = $db->prepare($meseroQuery);
+                $stmtMesero->execute();
+
+
+                if ($stmtMesero->rowCount() > 0) {
+
+                    header('Location: /pages/personal.php?success=1');
+                    exit;
+                } else {
+                    $errores[] = "Error al insertar en la tabla de mesero";
+                }
+            } else {
+                $errores[] = "Error al insertar el personal";
+            }
+        } else {
+
+            if ($stmt->rowCount() > 0) {
+                header('Location: /pages/personal.php?success=1');
+                exit;
+            } else {
+                $errores[] = "Error al insertar el personal";
+            }
         }
     }
 }
-
 ?>
 
 <main class="contenedor seccion contenido-centrado">
@@ -42,12 +69,12 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <?php endforeach; ?>
     <h1>Agregar Personal</h1>
 
-    <form class="formulario" method="POST"> 
+    <form class="formulario" method="POST">
         <fieldset>
             <legend>Información del Personal</legend>
 
             <label for="nombre">Nombre:</label>
-            <input type="text" name="nombre" placeholder="Nombre completo" id="nombre" required>
+            <input type="text" name="nombre" placeholder="Nombre completo" id="nombre" onchange="mostrarOcultarCampo()" required>
 
             <label for="rol">Rol:</label>
             <select name="rol" id="rol" required>
@@ -55,13 +82,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 <option value="Mesero">Mesero</option>
                 <option value="Chef">Chef</option>
                 <option value="Host">Host</option>
-                <option value="Gerente">Gerente</option>s
+                <option value="Gerente">Gerente</option>
             </select>
+
+            <div class="campoMesero">
+                <label for="area">ID área asignada:</label>
+                <input type="number" name="area" placeholder="ID área asignada" id="area">
+            </div>
+
         </fieldset>
 
         <input type="submit" value="Crear" class="boton-verde">
     </form>
 
-<?php
-incluirTemplate('footer');
-?>
+    <script src="../app.js"></script>
+
+    <?php
+    incluirTemplate('footer');
+    ?>
