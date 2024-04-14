d6b708f05c7e7fa1daeb0d7be282c8c8a9beacbc
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-04-13 22:39:57 -0600

    Botones

--- pages/reportes.php
+++ pages/reportes.php
@@ -5,13 +5,13 @@ require '../includes/funciones.php';
 incluirTemplate('header', $inicio = true);
 ?>
 
-<main class="pedidos">
-<button class="boton-verde">+ Agregar reporte de platos más pedidos</button>
-<button class="boton-verde">+ Agregar reporte de horario con más pedidos</button>
-<button class="boton-verde">+ Agregar reporte de promedio de consumo</button>
-<button class="boton-verde">+ Agregar reporte de quejas por persona</button>
-<button class="boton-verde">+ Agregar reporte de quejas por plato</button> 
-<button class="boton-verde">+ Agregar reporte de eficiencia de meseros</button>  
+<main class="reportes">
+<button class="boton-reporte-platos">+ Agregar reporte de platos más pedidos</button>
+<button class="boton-reporte-horario">+ Agregar reporte de horario con más pedidos</button>
+<button class="boton-reporte-promedio">+ Agregar reporte de promedio de consumo</button>
+<button class="boton-reporte-quejasper">+ Agregar reporte de quejas por persona</button>
+<button class="boton-reporte-quejasp">+ Agregar reporte de quejas por plato</button> 
+<button class="boton-reporte-eficiencia">+ Agregar reporte de eficiencia de meseros</button>  
 </main>
 
 <?php
