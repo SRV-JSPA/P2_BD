34e51394f754bff1c506e06fe8d192e14f6f568e
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-04-13 22:51:31 -0600

    Dirección botones

--- pages/reportes.php
+++ pages/reportes.php
@@ -6,12 +6,12 @@ incluirTemplate('header', $inicio = true);
 ?>
 
 <main class="reportes">
-<button class="boton-reporte-platos">+ Agregar reporte de platos más pedidos</button>
-<button class="boton-reporte-horario">+ Agregar reporte de horario con más pedidos</button>
-<button class="boton-reporte-promedio">+ Agregar reporte de promedio de consumo</button>
-<button class="boton-reporte-quejasper">+ Agregar reporte de quejas por persona</button>
-<button class="boton-reporte-quejasp">+ Agregar reporte de quejas por plato</button> 
-<button class="boton-reporte-eficiencia">+ Agregar reporte de eficiencia de meseros</button>  
+<button class="boton-reporte-platos" onclick="window.location.href = 'form-reporte-platos.php';" >+ Agregar reporte de platos más pedidos</button>
+<button class="boton-reporte-horario" onclick="window.location.href = 'form-reporte-horario.php';" >+ Agregar reporte de horario con más pedidos</button>
+<button class="boton-reporte-promedio" onclick="window.location.href = 'form-reporte-promedio.php';" >+ Agregar reporte de promedio de consumo</button>
+<button class="boton-reporte-quejasper" onclick="window.location.href = 'form-reporte-quejasper.php';" >+ Agregar reporte de quejas por persona</button>
+<button class="boton-reporte-quejasp" onclick="window.location.href = 'form-reporte-quejasp.php';" >+ Agregar reporte de quejas por plato</button> 
+<button class="boton-reporte-eficiencia" onclick="window.location.href = 'form-reporte-eficiencia.php';" >+ Agregar reporte de eficiencia de meseros</button>  
 </main>
 
 <?php
