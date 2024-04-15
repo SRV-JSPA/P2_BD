f1f668e197a97e21e3ca23782ad5c8808d6cd570
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-04-14 18:24:49 -0600

    Form reporte horario con más pedidos

--- pages/form-reporte-horario.php
+++ pages/form-reporte-horario.php
@@ -6,6 +6,17 @@ incluirTemplate('header', $inicio = true);
 ?>
 
 <main class="form-reporte-horario">
+    <h2>Reporte de horario en el que se ingresan más pedidos</h2>
+
+    <form method="post">
+        <label for="fecha_inicio">Fecha de Inicio:</label>
+        <input type="date" name="fecha_inicio" id="fecha_inicio" required>
+        <div> </div>
+        <label for="fecha_fin">Fecha de Fin:</label>
+        <input type="date" name="fecha_fin" id="fecha_fin" required>
+
+        <input type="submit" value="Generar Reporte" class="boton-verde">
+    </form>
 </main>
 
 <?php
