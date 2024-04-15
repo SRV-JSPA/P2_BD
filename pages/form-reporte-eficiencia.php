28ed18adbafa4b2de5cae7fd84686957801a7de3
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-04-14 18:26:27 -0600

    Form reporte eficiencia

--- pages/form-reporte-eficiencia.php
+++ pages/form-reporte-eficiencia.php
@@ -6,6 +6,17 @@ incluirTemplate('header', $inicio = true);
 ?>
 
 <main class="form-reporte-eficiencia">
+    <h2>Reporte de eficiencia de meseros mostrando los resultados de las encuestas</h2>
+
+    <form method="post">
+        <label for="fecha_inicio">Fecha de Inicio:</label>
+        <input type="date" name="fecha_inicio" id="fecha_inicio" required>
+        <div> </div>
+        <label for="fecha_fin">Fecha de Fin:</label>
+        <input type="date" name="fecha_fin" id="fecha_fin" required>
+
+        <input type="submit" value="Generar Reporte" class="boton-verde">
+    </form>
 </main>
 
 <?php
